fd23ace67b2ad9d524aad16a59375a08273f2103
Quang Tran Minh <quang.tranminh@hanoi.git>
2025-03-30 05:31:58 +0700

    clean project

--- android/app/src/main/kotlin/com/example/magical_stories/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.magical_stories
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/config/env.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
-class Env {
-  static String get googleAiApiKey => dotenv.env['GOOGLE_AI_API_KEY'] ?? '';
-
-  static const String googleAiEndpoint =
-      'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent';
-}
